2fda0866cd211f49095e4b6ad4c4e8a709a29ce1
Unal Focadan <unal.focadan@gmail.com>
2024-07-08 23:26:50 +0300

    08072024 Commit

--- AIImageValidator/ContentView.swift
+++ AIImageValidator/ContentView.swift
@@ -9,62 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     @EnvironmentObject var authViewModel: AuthViewModel
-    @State private var image: UIImage?
-    @State private var showImagePicker = false
-    @State private var uploadURL: URL?
 
     var body: some View {
         if authViewModel.isSignedIn {
-            VStack {
-                if let image = image {
-                    Image(uiImage: image)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 200, height: 200)
-                } else {
-                    Image(systemName: "photo")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 200, height: 200)
-                }
-
-                Button("Select Image") {
-                    showImagePicker = true
-                }
-                .padding()
-
-                if image != nil {
-                    Button("Upload Image") {
-                        uploadImage()
-                    }
-                    .padding()
-                }
-
-                if let uploadURL = uploadURL {
-                    Text("Image uploaded to: \(uploadURL.absoluteString)")
-                        .padding()
-                }
-
-                Button("Sign Out") {
-                    authViewModel.signOut()
-                }
-                .padding()
-            }
-            .sheet(isPresented: $showImagePicker) {
-                ImagePicker(image: $image)
-            }
-            .padding()
+            MainView() // Your main view after login
         } else {
             AuthView()
-        }
-    }
-
-    func uploadImage() {
-        guard let image = image else { return }
-        StorageManager().uploadImage(image) { url in
-            if let url = url {
-                self.uploadURL = url
-            }
         }
     }
 }
